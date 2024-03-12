c6cc11d4254443b7ff2fd60fdca38a772d8cb524
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2024-03-12 20:05:45 +0100

    #3 add: added button and started on logic

---
+++ scroll.js
@@ -0,0 +1,64 @@
+const post = document.querySelector("#main-content");
+const comments = document.querySelector("#comment-tree");
+console.log("COMMENTS IS " + comments);
+
+const targetNode = document.querySelector("#main-content");
+
+if (comments === null) {
+  const addIdsToComments = () => {
+    console.log("addIdsToComments");
+    const comments = document.querySelector("#comment-tree");
+    let count = 1;
+    comments.childNodes.forEach((node, index) => {
+      if (node.nodeName === "SHREDDIT-COMMENT") {
+        node.id = `comment-${count}`;
+        count++;
+      }
+    });
+  };
+
+  const observer = new MutationObserver((mutationList) => {
+    console.log(mutationList);
+    mutationList.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        if (node.id.includes("comment-tree-content-anchor-")) {
+          addIdsToComments();
+        }
+      });
+    });
+  });
+  observer.observe(targetNode, {
+    childList: true,
+  });
+} else {
+  console.log("COMMENTS EXIST");
+  let count = 1;
+  comments.childNodes.forEach((node, index) => {
+    if (node.nodeName === "SHREDDIT-COMMENT") {
+      node.id = `comment-${count}`;
+      count++;
+    }
+  });
+}
+
+const createDomElement = (html) => {
+  const dom = new DOMParser().parseFromString(html, "text/html");
+  return dom.body.firstElementChild;
+};
+
+const scrollToNext = () => {
+  console.log("SCROLLING!!!!");
+  location.href = "#";
+  location.href = "#comment-1";
+};
+
+const scrollBtn = createDomElement(`
+    <button id="scrollToNextButton" type="button" class="scrollBtn centerText">
+      V
+    </button>
+  `);
+
+post.prepend(scrollBtn);
+
+const scrollToNextButton = document.querySelector("#scrollToNextButton");
+scrollToNextButton.addEventListener("click", scrollToNext);
