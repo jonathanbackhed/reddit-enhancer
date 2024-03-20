723d763c259be7b300ee9c9855e279e675ef280d
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2024-03-20 01:16:01 +0100

    #5 fix: refactoring started

--- scroll.js
+++ scroll.js
@@ -1,31 +1,13 @@
-const post = document.querySelector("#main-content");
-const comments = document.querySelector("#comment-tree");
-console.log("COMMENTS IS " + comments);
-
 const NAVBAR_HEIGHT = 56;
-let commentIndex = 1;
+let scrollIndex = 1;
 
-const targetNode = document.querySelector("#main-content");
-
-if (comments === null) {
-  const addIdsToComments = () => {
-    console.log("addIdsToComments");
-    const comments = document.querySelector("#comment-tree");
-    let count = 1;
-    comments.childNodes.forEach((node, index) => {
-      if (node.nodeName === "SHREDDIT-COMMENT") {
-        node.id = `comment-${count}`;
-        count++;
-      }
-    });
-  };
-
+const runFunctionOnObserve = (elementId, targetNode, callback) => {
   const observer = new MutationObserver((mutationList) => {
     console.log(mutationList);
     mutationList.forEach((mutation) => {
       mutation.addedNodes.forEach((node) => {
-        if (node.id.includes("comment-tree-content-anchor-")) {
-          addIdsToComments();
+        if (node.id.includes(elementId)) {
+          callback();
         }
       });
     });
@@ -33,8 +15,11 @@ if (comments === null) {
   observer.observe(targetNode, {
     childList: true,
   });
-} else {
-  console.log("COMMENTS EXIST");
+};
+
+const addIdsToComments = () => {
+  console.log("addIdsToComments");
+  const comments = document.querySelector("#comment-tree");
   let count = 1;
   comments.childNodes.forEach((node, index) => {
     if (node.nodeName === "SHREDDIT-COMMENT") {
@@ -42,7 +27,10 @@ if (comments === null) {
       count++;
     }
   });
-}
+};
+
+// let targetNode = document.querySelector("#main-content");
+// runFunctionOnObserve("comment-tree-content-anchor-", targetNode, addIdsToComments);
 
 // SCROLL BTN & LOGIC
 const createDomElement = (html) => {
@@ -55,16 +43,62 @@ const scrollToNext = () => {
     top: cmt.offsetTop - NAVBAR_HEIGHT,
     behavior: "smooth",
   });
-  commentIndex++;
+  scrollIndex++;
 };
 
-const scrollBtn = createDomElement(`
-    <button id="scrollToNextButton" type="button" class="scrollBtn centerText">
-      V
-    </button>
-  `);
+// X 1. wait for navigation event to /r/
+// 2. wait for comments to load
+// 3. run addIdsToComments
+// 4. add scroll button
 
-post.prepend(scrollBtn);
+window.addEventListener("popstate", (event) => {
+  if (location.pathname.startsWith("/r/")) {
+    console.log("YOU'RE ON A POST PAGE");
 
-const scrollToNextButton = document.querySelector("#scrollToNextButton");
-scrollToNextButton.addEventListener("click", scrollToNext);
+    let asd = document.querySelector("#main-content");
+    console.log(asd);
+
+    const observer = new MutationObserver((mutationList) => {
+      console.log(mutationList);
+      // mutationList.forEach((mutation) => {
+      //   mutation.addedNodes.forEach((node) => {
+      //     if (node.id.includes(elementId)) {
+      //       callback();
+      //     }
+      //   });
+      // });
+    });
+    observer.observe(asd, {
+      childList: true,
+    });
+  } else {
+    console.log("YOU'RE NOT ON A POST PAGE");
+  }
+});
+
+// window.addEventListener("popstate", (event) => {
+//   console.log("POPSTATE EVENT");
+//   if (location.pathname.startsWith("/r/")) {
+//     // you're on a post page
+//     console.log("YOU'RE ON A POST PAGE");
+//     scrollIndex = 1;
+
+//     const post = document.querySelector("#main-content");
+//     const comments = document.querySelector("#comment-tree");
+
+//     const scrollBtn = createDomElement(`
+//     <button id="scrollToNextButton" type="button" class="scrollBtn centerText">
+//       V
+//     </button>
+//   `);
+
+//     post.prepend(scrollBtn);
+
+//     const scrollToNextButton = document.querySelector("#scrollToNextButton");
+//     scrollToNextButton.addEventListener("click", scrollToNext);
+//   } else {
+//     console.log("YOU'RE NOT ON A POST PAGE");
+//     // var element = document.querySelector("#scrollToNextButton");
+//     // element.parentNode.removeChild(element);
+//   }
+// });
